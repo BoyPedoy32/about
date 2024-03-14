16dd78f998377dae2c4cd7269cef3d27de0b96d3
BoyPedoy32 <163428943+BoyPedoy32@users.noreply.github.com>
2024-03-14 15:18:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Animacja nagłówka
+const header = document.querySelector('header h1');
+
+header.addEventListener('mouseover', function() {
+    this.style.color = 'blue';
+});
+
+header.addEventListener('mouseout', function() {
+    this.style.color = '#333';
+});
